0d7588e97a455fbc3d078b062bf087a80541af37
Lars Skodje <lskodje@isilon.com>
2022-07-12 02:11:26 -0700

    Add documentation

---
+++ docs/index.md
@@ -0,0 +1,6 @@
+A tiny module for parsing repeating textual patterns.
+=======================================================
+
+The module is intented to provide an abstracted way of generating regular
+expressions to parse repeating text, as often times is needed when parsing data
+from command line utilities.
--- grouper/__init__.py
+++ grouper/__init__.py
@@ -2,19 +2,31 @@
 
 import re
 from dataclasses import dataclass
+from typing import List, Dict
 
 
 @dataclass
 class Field:
+    """Class to identify a key/value pair by regular expression.
+
+    Arguments:
+        pattern (str): Main pattern identifying the field to look for.
+        separator (str): Pattern that separator the identifying string from the
+            value.
+        leading (str): Optional pattern indicating how the string starts.
+        specifier (str): The regular expression group name.
+        value (str): Regular expression identifying the value.
+        optional (bool): Boolean indicating whether the field is optional.
+
+    """
     pattern: str
-    sep: str = r"\s*:\s*"  # E.g.: ' : '
+    separator: str = r"\s*:\s*"
     leading: str = r"\s*"
     specifier: str = None
     value: str = None
     optional: bool = False
-    ignorecase: bool = False
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if not self.specifier:
             self.specifier = self.pattern.lower().replace(" ", "_")
         if not self.value:
@@ -22,7 +34,7 @@ class Field:
         regstr = (
             rf"^{self.leading}"
             rf"{self.pattern}"
-            rf"{self.sep}"
+            rf"{self.separator}"
             rf"(?P<{self.specifier}>{self.value})\n"
         )
         if self.optional:
@@ -35,18 +47,29 @@ class Field:
 @dataclass
 class Parser:
     fields: Field
-    sep: str = ""
+    separator: str = ""
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self.regex = re.compile(
             "%s" % (".*?".join(field.regex.pattern for field in self.fields)),
             re.MULTILINE | re.DOTALL,
         )
 
-    def parse(self, string):
-        """Iterate over each substring"""
+    def parse(self, string: str) -> List[Dict[str, str]]:
+        """Parse string and produce groupings.
+
+        Iterate over each substring separated by our separator and turn them
+        into a list of dicts based on our regular expressin.
+
+
+        Arguments:
+            string (str): The string to parse.
+
+        Returns:
+            list: A list of dicts.
+        """
         return [
             mtch.groupdict()
-            for substr in re.split(self.sep, string)
+            for substr in re.split(self.separator, string)
             if (mtch := self.regex.search(substr))
         ]
